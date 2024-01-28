18904b149d48702e98998dfaa2bbbb19fff87d07
rahatfaruk <rahatfaruk99@gmail.com>
2024-01-28 22:06:03 +0600

    update todo feature added

--- script.js
+++ script.js
@@ -14,18 +14,17 @@ addTodoForm.addEventListener('submit',  e => {
     return
   }
 
-  const newTodo = {id: Date.now(), text: todoText, isDone: false}
+  const newTodo = {id: `${Date.now()}`, text: todoText, isDone: false}
   const todoHtml = `
     <li class="bg-gray-200 px-2 rounded-lg flex items-center" data-id=${newTodo.id}>
       <input type="checkbox" name="done" class="mx-2 hover:cursor-pointer" ${newTodo.isDone && 'checked'}>
       <input type="text" name="text" class="flex-1 bg-transparent px-2 py-2" value=${newTodo.text}>
-      <button class="edit-btn bg-green-30 px-3 py-2 hover:opacity-75"><i class="bi bi-pencil-square"></i></button>
-      <button class="delete-btn text-red-600 px-3 py-2 hover:opacity-75"><i class="bi bi-trash-fill"></i></button>
+      <button class="edit-btn bg-green-30 px-3 py-2 hover:opacity-75"><i class="bi bi-pencil-square pointer-events-none"></i></button>
+      <button class="delete-btn text-red-600 px-3 py-2 hover:opacity-75"><i class="bi bi-trash-fill pointer-events-none"></i></button>
     </li>
   `
-  todoList.innerHTML += todoHtml
+  todoList.insertAdjacentHTML('beforeend', todoHtml)
   todos.push(newTodo)
-  console.log(todos);
 })
 
 // ## clicked inside todo
@@ -35,6 +34,20 @@ todoList.addEventListener('click', e => {
     const todoEl = e.target.parentElement
     todos.splice(todos.findIndex(todo => todo.id === todoEl.dataset.id), 1)
     todoEl.remove()
-    console.log(todos);
   }
+
+  // ### update todo: check/uncheck
+  if (e.target.type === 'checkbox') {
+    // Note: checkbox ui aumatically get updated
+    const todoId = e.target.parentElement.dataset.id
+    todos.find(todo => todo.id === todoId).isDone = e.target.checked
+  }
+
+  // ## update todo: text
+  if (e.target.classList.contains('edit-btn')) {
+    const newText = e.target.previousElementSibling.value
+    const todoId = e.target.parentElement.dataset.id
+    todos.find(todo => todo.id === todoId).text = newText
+  }
+
 })
